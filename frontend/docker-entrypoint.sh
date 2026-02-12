a653dc427b611cd85ef203cacb6eae652d8b5266
pjnp5 <paulojnpinto02@ua.pt>
2026-02-12 17:22:06 +0000

    Update Docker entrypoint and nginx config to support dynamic PORT variable

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 set -e
 
-# Default backend URL for local Docker Compose
+# Default values for local Docker Compose
 BACKEND_URL="${BACKEND_URL:-http://backend:5050}"
+PORT="${PORT:-80}"
 
-echo "Configuring nginx with BACKEND_URL: $BACKEND_URL"
+echo "Configuring nginx with:"
+echo "  BACKEND_URL: $BACKEND_URL"
+echo "  PORT: $PORT"
 
-# Substitute environment variable in nginx config
-envsubst '${BACKEND_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+# Substitute environment variables in nginx config
+envsubst '${BACKEND_URL} ${PORT}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 
 # Start nginx
 exec nginx -g 'daemon off;'
